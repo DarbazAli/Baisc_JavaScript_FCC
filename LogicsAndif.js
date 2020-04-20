cfed11e91261fce024484870cdbecba5e4f8cb24
Darbaz Ali <darbaz.a.h@gmail.com>
2020-04-20 22:07:53 +0300

    if_else_if Done

--- LogicsAndif.js
+++ LogicsAndif.js
@@ -212,6 +212,145 @@ function checkEligible(age) {
 }
 
 
+/* 
+    Defingin Control flow of a programs with (if-else) statement
+
+    if (condition is true) {
+        execute this line
+    } else {
+        execute this line
+    }
+*/
+
+// Single if with else
+
+function checkTrueOrFalse(num) {
+    if ( num > 10) {
+    return "I must be true";
+    } else {
+    return "I must be false";
+    }
+}
+
+
+
+// More thatn one if
+/* 
+    if (condition one is true) {
+        execute this line
+    }
+    else if (condition two is true ) {
+        execute this line
+    }
+    else {
+        execute this line
+    }
+*/
+
+function checkNumber(number) {
+    if ( number > 15 ) {
+        return "Bigger than 15";
+    }
+    else if (number < 5 ) {
+        return "Smaller than 5";
+    }
+    else {
+        return "Between 5 and 15";
+    }
+}
+
+
+
+
+/* 
+    Chaining If Else Statements
+    if/else statements can be chained together for complex logic.
+
+        if (condition1) {
+        statement1
+        } else if (condition2) {
+        statement2
+        } else if (condition3) {
+        statement3
+        . . .
+        } else {
+        statementN
+        }
+*/
+
+function checkSize(size) {
+    if ( size < 5 ) {
+        return "Tiny";
+    } else if ( size < 10) {
+        return "Small";
+    } else if ( size < 15 ) {
+        return "Medium";
+    } else {
+        return "Large";
+    }
+}
+
+console.log(checkSize(10));
+
+
+
+
+/* 
+    Algorithm: Golf Code
+    Strokes	Return
+    1	"Hole-in-one!"
+    <= par - 2	"Eagle"
+    par - 1	"Birdie"
+    par	"Par"
+    par + 1	"Bogey"
+    par + 2	"Double Bogey"
+    >= par + 3	"Go Home!"
+*/
+
+
+function golfScore(par, strokes) {
+    var names = 
+    ["Hole-in-one!", 
+    "Eagle", 
+    "Birdie", 
+    "Par", 
+    "Bogey", 
+    "Double Bogey", 
+    "Go Home!"];
+
+    // return first name, if stroke == 1
+   if ( strokes == 1 ) {
+     return names[0]
+   }
+  
+   else if (strokes == par ) {
+     return names[3]
+   }
+  
+   else if ( strokes <= par -2 ) {
+     return names[1]
+   }
+  
+  
+   else if ( strokes == par - 1 ) {
+     return names[2]
+   }
+  
+   else if ( strokes == par + 1) {
+     return names[4]
+   }
+  
+   else if ( strokes == par + 2) {
+     return names[5]
+   }
+  
+  
+    else {
+      return names[6];
+    }
+
+}
+
 
 
 
